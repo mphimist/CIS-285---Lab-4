10ea2d41a3d9c69b8fb379f3b6643fc708c53aaf
Margaret <margare@umich.edu>
2026-02-12 21:17:34 -0500

    Ran Program

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public static void main(String[] args)
     {
         for(int i=1;i<=10;i++) 
             {
-                System.out.println("Hello World... :)"+i);
+                System.out.println("Hello World... :) "+i);
 
                 //I don't know java so I'm just going to add this comment
 
